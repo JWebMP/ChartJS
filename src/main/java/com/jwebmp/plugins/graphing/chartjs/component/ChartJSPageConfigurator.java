9518886c5be6fb2631c90a474c2e6ac6f5d28534
GedMarc <marc.magon@gmail.com>
2024-02-24 21:53:56 +0200

    JWebMP Updates Pre Structures Update

--- src/main/java/com/jwebmp/plugins/graphing/chartjs/component/ChartJSPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/graphing/chartjs/component/ChartJSPageConfigurator.java
@@ -1,12 +1,11 @@
 package com.jwebmp.plugins.graphing.chartjs.component;
 
-import com.jwebmp.core.*;
+import com.jwebmp.core.Page;
+import com.jwebmp.core.base.angular.client.annotations.typescript.TsDependency;
+import com.jwebmp.core.services.IPageConfigurator;
+import jakarta.validation.constraints.NotNull;
 
-import com.jwebmp.core.base.angular.client.annotations.typescript.*;
-import com.jwebmp.core.services.*;
-import jakarta.validation.constraints.*;
-
-@TsDependency(value = "chart.js", version = "^4.1.2")
+@TsDependency(value = "chart.js", version = "^4.4.1")
 public class ChartJSPageConfigurator implements IPageConfigurator<ChartJSPageConfigurator>
 {
 	
